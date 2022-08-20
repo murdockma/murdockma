938fef784644b29dbe58e5373f66a31cb1461bef
Michael Murdock <47290536+murdockma@users.noreply.github.com>
2022-08-20 01:28:53 -0600

    Update user.py

--- user.py
+++ user.py
@@ -1,2 +1,14 @@
-def user():
-  return "DenverCoder1"
+class user:
+  
+  def __init__(self, path, user):
+    self.path = path
+    self.user = user
+    
+  def __eq__(self, other):
+    return self.path == other.path and self.user == other.user
+  
+  def __hash__(self):
+    return ('The hash is:')
+  
+  return str(hash((self.path, self.user)))
+  
